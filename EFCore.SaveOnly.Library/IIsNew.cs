75290ce2153ba75c5504528ea496bfa94f418330
adamfoneil <adamfoneil@proton.me>
2026-07-02 20:34:29 -0400

    grammar

--- EFCore.SaveOnly.Library/IIsNew.cs
+++ EFCore.SaveOnly.Library/IIsNew.cs
@@ -3,7 +3,7 @@
 public interface IIsNew
 {
     /// <summary>
-    /// how do we tell if an entity is "new" -- for example if it's int/long Id is 0
+    /// how do we tell if an entity is "new" -- for example if its int/long Id is 0
     /// </summary>
     Func<object, bool> IsNewConvention { get; }
 }
